0354b94cc42b76d17d94203862e8e558e521cdfd
dec0mpiled <Liimbix@gmail.com>
2016-01-29 20:37:28 +0000

    cookieshare

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,6 @@ var Post = mongoose.model('User', PostSchema);
 router.get('/', function(req, res, next) {
     Post.find({}, null, { sort: '-created' }, function (err, posts) {
         if (err) return next(err);
-        console.log(posts);
         res.render('index', { title: 'ShareCookie', posts: posts });
     });
 });
@@ -30,10 +29,18 @@ router.get('/help', function(req, res, next) {
 
 /* Post Cookie */
 router.post('/sharecookie', function(req, res, next) {
-    var badWord = /fuck|shit|cunt|damn|nigger|nigga|twat|dick|cum|tits|titties|boob|boobs|penis|cock|bbc|porn|pornography|rape|sex|orgasm|tit|raping|bitch|ass|clit|clitoris|breast|breasts/gi;
+    var badWord = /fuck|shit|cunt|damn|nigger|nigga|twat|dick|cum|tits|titties|boob|boobs|penis|cock|bbc|porn|pornography|rape|sex|orgasm|tit|raping|bitch|ass|clit|clitoris|breast|breasts|wigger/gi;
     var titleq=req.body.titlebox;
     var authorq=req.body.username;
     var contentq=req.body.texxtt;
+    
+    if(authorq=="drew") {
+        authorq="vanilla";
+    }
+    
+    if(authorq=="admin.name#1") {
+        authorq="drew";
+    }
     
 var mytitle = titleq;
 var mynewtitle = mytitle.toLowerCase();
@@ -46,6 +53,7 @@ mynewauthor = mynewauthor.replace(badWord,"****");
 var mycontent = contentq;
 var mynewcontent = mycontent.toLowerCase();
 mynewcontent = mynewcontent.replace(badWord,"****");
+mynewcontent = mynewcontent.replace("!",". Fuck!");
     
     var newpost = new Post({
         title: mynewtitle,
