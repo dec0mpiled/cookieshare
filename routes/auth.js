c0e28ad08d3ef93aa97e681824d9a4a394e8cdc6
dec0mpiled <Liimbix@gmail.com>
2016-02-11 15:00:25 +0000

    cookieshare

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post('/register', function(req, res, next) {
   User.register(new User({
     name: req.body.namebox,
     username: req.body.username,
-    following: ["drew",username],
+    following: ["drew"],
     bio: "No Bio Available",
     avatarurl: "https://medium.com/img/default-avatar.png",
     admin: false
